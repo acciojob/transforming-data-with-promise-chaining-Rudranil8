dcabf6cbcb2fe26b87bd1bd0c921f5e44d763093
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-08-29 17:01:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,12 +6,35 @@
 // 	y.innerText=`Result: ${x.value}`;
 // })
 
+// document.getElementById("btn").addEventListener("click", ()=>{
+//   new Promise(function(resolve, reject) {
+//     setTimeout(function() {
+//       let num = document.getElementById("ip").value;
+//       resolve(num);
+//     }, 2000);
+//   })
+//   .then(function(num) {
+//     let y = document.getElementById("output");
+//     y.innerText = `Result: ${num}`;
+//   });
+// });
+
 document.getElementById("btn").addEventListener("click", ()=>{
   new Promise(function(resolve, reject) {
     setTimeout(function() {
       let num = document.getElementById("ip").value;
       resolve(num);
     }, 2000);
+  })
+  .then(function(num) {
+    let y = document.getElementById("output");
+    y.innerText = `Result: ${num}`;
+    return new Promise(function(resolve, reject) {
+      setTimeout(function() {
+        num *= 2; // multiply the number by 2
+        resolve(num);
+      }, 1000);
+    });
   })
   .then(function(num) {
     let y = document.getElementById("output");
