202dc3d7e29f8e31c9e09139cc1a83ebaad4e665
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-08-29 16:57:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 //your JS code here. If required.
-let x=document.getElementById("ip");
-let y=document.getElementById("output");
+// let x=document.getElementById("ip");
+// let y=document.getElementById("output");
+// document.getElementById("btn").addEventListener("click", ()=>{
+// 	// alert(x.value);
+// 	y.innerText=`Result: ${x.value}`;
+// })
+
 document.getElementById("btn").addEventListener("click", ()=>{
-	// alert(x.value);
-	y.innerText=`Result: ${x.value}`;
-})
+  new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      let num = document.getElementById("ip").value;
+      resolve(num);
+    }, 2000);
+  })
+  .then(function(num) {
+    let y = document.getElementById("output");
+    y.innerText = `Result: ${num}`;
+  });
+});
