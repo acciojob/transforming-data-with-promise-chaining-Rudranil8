444c73cf04cb37df54a8e7de7c23174f7b2053fb
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-08-29 16:40:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+document.getElementById("btn").addEventListener("click", function() {
+	new Promise(function(resolve, reject) {
+  setTimeout(function() {
+    let num = document.getElementById("ip").value;
+    resolve(num);
+  }, 2000);
+})
+	.then(function(num) {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      num *= 2; // multiply the number by 2
+      resolve(num);
+    }, 1000);
+  });
+})
+});
